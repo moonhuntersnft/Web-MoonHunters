6c1cbdf616816f4e00398ac9726d04354acd1c2d
LeonardoSosa <leoesosag@gmail.com>
2022-04-12 18:11:19 -0300

    Modificação função que move slider
    =melhoria na função slider_move

--- web/js/main.js
+++ web/js/main.js
@@ -1,29 +1,21 @@
-function derecha(x){
+function slider_move(x){
     //Largura total = left(slider) + img + gap
     
     var imgWidth = x.clientWidth                                                                //Largura imagem
-
     var slider = document.getElementById("slider")                                              //Valor 'left' de .slider
     var leftValue = window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, "")
-
     var gap = parseInt(getComputedStyle(document.documentElement).fontSize)*3                   //Valor de 3rem(gap entre imagens)
     
-    var newposleft = (parseInt(leftValue) + imgWidth + gap)+"px"                                //Somar ('left' + imagem + gap)
 
-    slider.style.left = newposleft
-}
-
-function izquierda(x){
-    //Largura total = left(slider) - img - gap
-    
-    var imgWidth = x.clientWidth                                                                //Largura imagem
-
-    var slider = document.getElementById("slider")                                              //Valor 'left' de .slider
-    var leftValue = window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, "")
-
-    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)*3                   //Valor de 3rem(gap entre imagens)
-    
-    var newposleft = (parseInt(leftValue) - imgWidth - gap)+"px"                                //Subtrair ('left' - imagem - gap)
-
+    var cords = x.getBoundingClientRect();                                                      //Cordenadas de img no DOM
+    if(cords.right < screen.width && cords.left > 0){
+        return                                                                                  //Retornar se o slide está no meio da tela
+    }
+    else if(cords.right > window.screen.width){
+        var newposleft = (parseInt(leftValue) - imgWidth - gap)+"px"                            //Focar próximo slide
+    }
+    else {
+        var newposleft = (parseInt(leftValue) + imgWidth + gap)+"px"                            //Focar slide anterior
+    }
     slider.style.left = newposleft
 }
