3fbc09908a5318a0a3da13adb2a6580985ec8d02
LeonardoSosa <leoesosag@gmail.com>
2022-04-19 12:22:44 -0300

    Add Top collections, Popular cases
    +top collections
    +popular cases
    +função slider_auto
    
    Outros:
    =modificação na função slider_move para uma melhor legibilidade

--- web/js/main.js
+++ web/js/main.js
@@ -1,21 +1,70 @@
-function slider_move(x){
-    //Largura total = left(slider) + img + gap
-    
-    var imgWidth = x.clientWidth                                                                //Largura imagem
-    var slider = document.getElementById("slider")                                              //Valor 'left' de .slider
-    var leftValue = window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, "")
-    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)*3                   //Valor de 3rem(gap entre imagens)
-    
+setInterval(slider_auto, 7000);
 
-    var cords = x.getBoundingClientRect();                                                      //Cordenadas de img no DOM
-    if(cords.right < screen.width && cords.left > 0){
-        return                                                                                  //Retornar se o slide está no meio da tela
+function slider_auto(){
+    //Left values
+    var sliderWidthClient = document.getElementById("slider").clientWidth
+    var sliderWidth = document.getElementById("slider").scrollWidth;
+    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)
+
+    var leftActual = parseInt(window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, ""))
+    var leftMove = sliderWidth/(slider.childElementCount)+gap/2
+    var leftNew = 0
+
+    //Move right
+    leftNew = (leftActual - leftMove)+"px"
+
+    //Move beginning
+    //! UNFINISHED !//
+    //! Conseguir el valor left máximo posible independiente de la cantidad de banners !//
+    leftMax = sliderWidthClient*(-3.00)
+    if(leftNew.replace(/px/,"") < leftMax){
+        leftNew = sliderWidthClient*(-0.65)+"px"
     }
-    else if(cords.right > window.screen.width){
-        var newposleft = (parseInt(leftValue) - imgWidth - gap)+"px"                            //Focar próximo slide
+
+    slider.style.left = leftNew
+}
+
+function slider_move(x){
+    //Left values
+    var sliderWidthClient = document.getElementById("slider").clientWidth
+    var sliderWidth = document.getElementById("slider").scrollWidth;
+    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)
+
+    var leftActual = parseInt(window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, ""))
+    var leftMove = sliderWidth/(slider.childElementCount)+gap/2
+    var leftNew = 0
+
+    //Go to beginning/Go to end
+    if(x.classList.contains("toLast")){
+        //! UNFINISHED !//
+        //! Conseguir el valor left máximo posible independiente de la cantidad de banners !//
+        leftNew = sliderWidthClient*(-3.00)+"px"
+        slider.style.left = leftNew
+        return
+    }
+    else if(x.classList.contains("toFirst")){
+        leftNew = sliderWidthClient*(-0.65)+"px"
+        slider.style.left = leftNew
+        return
+    }
+
+    var bannerCords = x.getBoundingClientRect();
+    if(bannerCords.right < screen.width && bannerCords.left > 0){
+        //Don't move
+        return
+    }
+    else if(bannerCords.right > window.screen.width){
+        //Move right
+        leftNew = (leftActual - leftMove)+"px"
     }
     else {
-        var newposleft = (parseInt(leftValue) + imgWidth + gap)+"px"                            //Focar slide anterior
+        //Move left
+        leftNew = (leftActual + leftMove)+"px"
     }
-    slider.style.left = newposleft
+    
+    slider.style.left = leftNew
 }
+
+
+// console.log(slider.childElementCount)   //Number of direct childs
+// console.log(slider.childNodes.length)   //Number of childs, grandchilds...
