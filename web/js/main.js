d0ba0cee5e119b2e6c1dd3e92a12e42e961724be
Leonardo Sosa <leoesosag@gmail.com>
2022-05-01 17:35:31 -0300

    Add Last News, Footer
    +last news
    +footer
    +pagina perfil
    
    Outros:
    =mudança nos arquivos/pastas de css
    =mudanças nomes de classes
    +settings.json

--- web/js/main.js
+++ web/js/main.js
@@ -3,25 +3,27 @@ setInterval(slider_auto, 7000);
 function slider_auto(){
     //Left values
     var sliderWidthClient = document.getElementById("slider").clientWidth
-    var sliderWidth = document.getElementById("slider").scrollWidth;
-    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)
+    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)*3
 
     var leftActual = parseInt(window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, ""))
-    var leftMove = sliderWidth/(slider.childElementCount)+gap/2
+    var leftMove = document.getElementById("slide").clientWidth+gap+0.3
     var leftNew = 0
 
     //Move right
-    leftNew = (leftActual - leftMove)+"px"
-
-    //Move beginning
+    leftNew = (leftActual - leftMove)
     //! UNFINISHED !//
+    //! Tamanho das imagens é proporcional à quantidade de imagens !//
     //! Conseguir el valor left máximo posible independiente de la cantidad de banners !//
     leftMax = sliderWidthClient*(-3.00)
-    if(leftNew.replace(/px/,"") < leftMax){
-        leftNew = sliderWidthClient*(-0.65)+"px"
+    // leftMax = sliderWidthClient*(-(slider.childElementCount-3))
+
+
+    if(leftNew < leftMax){
+        leftNew = sliderWidthClient*(-0.65)
+        //? leftNew = sliderWidthClient*(-0.00)
     }
 
-    slider.style.left = leftNew
+    slider.style.left = leftNew+"px"
 }
 
 function slider_move(x){
@@ -37,14 +39,16 @@ function slider_move(x){
     //Go to beginning/Go to end
     if(x.classList.contains("toLast")){
         //! UNFINISHED !//
+        //! Tamanho das imagens é proporcional à quantidade de imagens !//
         //! Conseguir el valor left máximo posible independiente de la cantidad de banners !//
-        leftNew = sliderWidthClient*(-3.00)+"px"
-        slider.style.left = leftNew
+        leftNew = sliderWidthClient*(-3.00)
+
+        slider.style.left = leftNew+"px"
         return
     }
     else if(x.classList.contains("toFirst")){
-        leftNew = sliderWidthClient*(-0.65)+"px"
-        slider.style.left = leftNew
+        leftNew = sliderWidthClient*(-0.65)
+        slider.style.left = leftNew+"px"
         return
     }
 
@@ -55,16 +59,18 @@ function slider_move(x){
     }
     else if(bannerCords.right > window.screen.width){
         //Move right
-        leftNew = (leftActual - leftMove)+"px"
+        leftNew = (leftActual - leftMove)
     }
     else {
         //Move left
-        leftNew = (leftActual + leftMove)+"px"
+        leftNew = (leftActual + leftMove)
     }
     
-    slider.style.left = leftNew
+    slider.style.left = leftNew+"px"
 }
 
 
 // console.log(slider.childElementCount)   //Number of direct childs
 // console.log(slider.childNodes.length)   //Number of childs, grandchilds...
+
+// document.getElementById("slider").scrollWidth   //Tamaño real máximo de id=slider
