301e91e10d475d7f5425ce4e633c9dd4a5a023e9
Leonardo Sosa <leoesosag@gmail.com>
2022-05-16 18:31:28 -0300

    Change Slider
    index.html:
    =melhoras no slider, que foi reescrito do zero

--- web/js/main.js
+++ web/js/main.js
@@ -1,76 +1,44 @@
 setInterval(slider_auto, 7000);
-
-function slider_auto(){
-    //Left values
-    var sliderWidthClient = document.getElementById("slider").clientWidth
-    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)*3
-
-    var leftActual = parseInt(window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, ""))
-    var leftMove = document.getElementById("slide").clientWidth+gap+0.3
-    var leftNew = 0
-
-    //Move right
-    leftNew = (leftActual - leftMove)
-    //! UNFINISHED !//
-    //! Tamanho das imagens é proporcional à quantidade de imagens !//
-    //! Conseguir el valor left máximo posible independiente de la cantidad de banners !//
-    leftMax = sliderWidthClient*(-3.00)
-    // leftMax = sliderWidthClient*(-(slider.childElementCount-3))
-
-
-    if(leftNew < leftMax){
-        leftNew = sliderWidthClient*(-0.65)
-        //? leftNew = sliderWidthClient*(-0.00)
+function slider_auto() {
+    const slider = document.querySelector(".slider")
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
+    if(sliderIndex == 4) {
+        slider.style.setProperty("--slider-index", sliderIndex -3)
+        return
     }
-
-    slider.style.left = leftNew+"px"
+    slider.style.setProperty("--slider-index", sliderIndex +1)
 }
 
-function slider_move(x){
-    //Left values
-    var sliderWidthClient = document.getElementById("slider").clientWidth
-    var sliderWidth = document.getElementById("slider").scrollWidth;
-    var gap = parseInt(getComputedStyle(document.documentElement).fontSize)
+document.addEventListener("click", e => {
+    let handle
+    if(e.target.matches(".handle")) {
+        handle = e.target
+    } else {
+        handle = e.target.closest(".handle")
+    }
+    if (handle != null) {
+        onHandleClick(handle)
+    }
+})
 
-    var leftActual = parseInt(window.getComputedStyle(slider).getPropertyValue("left").replace(/px/, ""))
-    var leftMove = sliderWidth/(slider.childElementCount)+gap/2
-    var leftNew = 0
 
-    //Go to beginning/Go to end
-    if(x.classList.contains("toLast")){
-        //! UNFINISHED !//
-        //! Tamanho das imagens é proporcional à quantidade de imagens !//
-        //! Conseguir el valor left máximo posible independiente de la cantidad de banners !//
-        leftNew = sliderWidthClient*(-3.00)
+function onHandleClick(handle) {
+    const slider = document.querySelector(".slider")
+    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
 
-        slider.style.left = leftNew+"px"
-        return
-    }
-    else if(x.classList.contains("toFirst")){
-        leftNew = sliderWidthClient*(-0.65)
-        slider.style.left = leftNew+"px"
-        return
+    if(handle.classList.contains("left-handle")) {
+        if(sliderIndex == 1) {
+            slider.style.setProperty("--slider-index", sliderIndex +3)
+            return
+        }
+        slider.style.setProperty("--slider-index", sliderIndex -1)
     }
 
-    var bannerCords = x.getBoundingClientRect();
-    if(bannerCords.right < screen.width && bannerCords.left > 0){
-        //Don't move
-        return
+    if(handle.classList.contains("right-handle")) {
+        if(sliderIndex == 4) {
+            slider.style.setProperty("--slider-index", sliderIndex -3)
+            return
+        }
+        slider.style.setProperty("--slider-index", sliderIndex +1)
     }
-    else if(bannerCords.right > window.screen.width){
-        //Move right
-        leftNew = (leftActual - leftMove)
-    }
-    else {
-        //Move left
-        leftNew = (leftActual + leftMove)
-    }
-    
-    slider.style.left = leftNew+"px"
 }
-
-
-// console.log(slider.childElementCount)   //Number of direct childs
-// console.log(slider.childNodes.length)   //Number of childs, grandchilds...
-
-// document.getElementById("slider").scrollWidth   //Tamaño real máximo de id=slider
