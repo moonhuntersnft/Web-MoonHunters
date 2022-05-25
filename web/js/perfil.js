fa145c7382534195b29d0f01de7b67bbee241481
Leonardo Sosa <leoesosag@gmail.com>
2022-05-25 17:50:35 -0300

    Add Achievements, Friends, Comments, Gallery -> slider
    perfil.html:
    +achievements
    +friends
    +comments
    =gallery is now a slider
    
    Outros:
    =links do header agora redirecionam para sua respectiva página
    index.html:
    =imagens do slider agora são links
    =fix da função sliderAuto(), agora funciona corretamente
    perfil.html:
    =itens do trending e news agora são links
    =fix svg da foto de perfil (bordas redondas)
    ='Showcase' em footer de containers agora é um link

--- web/js/perfil.js
+++ web/js/perfil.js
@@ -1,15 +1,3 @@
-let intervalSliderAuto = setInterval(sliderAuto, 7000);
-
-function sliderAuto() {
-    const slider = document.querySelector(".slider")
-    const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
-    if(sliderIndex == 4) {
-        slider.style.setProperty("--slider-index", sliderIndex -3)
-        return
-    }
-    slider.style.setProperty("--slider-index", sliderIndex +1)
-}
-
 document.addEventListener("click", e => {
     let handle
     if(e.target.matches(".handle")) {
@@ -19,27 +7,22 @@ document.addEventListener("click", e => {
     }
     if (handle != null) {
         onHandleClick(handle)
-        clearInterval(intervalSliderAuto)
-        intervalSliderAuto = setInterval(sliderAuto, 7000);
     }
 })
-
 
 function onHandleClick(handle) {
     const slider = document.querySelector(".slider")
     const sliderIndex = parseInt(getComputedStyle(slider).getPropertyValue("--slider-index"))
 
     if(handle.classList.contains("left-handle")) {
-        if(sliderIndex == 1) {
-            slider.style.setProperty("--slider-index", sliderIndex +3)
+        if(sliderIndex == 0) {
             return
         }
         slider.style.setProperty("--slider-index", sliderIndex -1)
     }
 
     if(handle.classList.contains("right-handle")) {
-        if(sliderIndex == 4) {
-            slider.style.setProperty("--slider-index", sliderIndex -3)
+        if(sliderIndex == 2) {
             return
         }
         slider.style.setProperty("--slider-index", sliderIndex +1)
